6942a848c832cd6dff505fe8ea3aabae50f829f7
redyel <lianis.ee7@gmail.com>
2025-09-18 10:41:47 +0530

    home page merge conflicts resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,16 @@ import Home from "./components/home/Home";
 import MoodTracker from "./components/mood-tracker/MoodTracker";
 import RootLayout from "./RootLayout";
 import AIGenerationScreen from "./components/study-enhance/AIGenerationScreen";
-import MindMapView from "./components/study-enhance/MindMapView";
 import FlashcardsLayout from "./components/study-enhance/FlashcardsLayout";
 import MindMapView from "./components/study-enhance/MindMapView";
-import RoomList from './components/room/RoomList';
-import VideoCall from './components/room/VideoCall';
+import RoomList from "./components/room/RoomList";
+import VideoCall from "./components/room/VideoCall";
 
-// Import your Resource Hub components
-import AllResources from './components/all-resources/AllResources';
-import PdfReader from './components/pdf-reader/PdfReader';
-import UploadPage from './components/upload-page/UploadPage';
-import MyLibrary from './components/myLibrary/MyLibrary';
+// Resource Hub components
+import AllResources from "./components/all-resources/AllResources";
+import PdfReader from "./components/pdf-reader/PdfReader";
+import UploadPage from "./components/upload-page/UploadPage";
+import MyLibrary from "./components/myLibrary/MyLibrary";
 import GroupResources from "./components/group-resources/GroupResources";
 import TrendingPage from "./components/trending-page/TrendingPage";
 
@@ -24,55 +23,34 @@ function App() {
     {
       path: "/",
       element: <RootLayout />,
-
       children: [
+        // Landing page
         { index: true, element: <Home /> },
-        { path: "/mood-tracker", element: <MoodTracker /> },
+
+        // Mood Tracker
+        { path: "mood-tracker", element: <MoodTracker /> },
+
+        // Study Enhance section
         {
-          path: "/study-enhance",
+          path: "study-enhance",
           children: [
-            {
-              index: true,
-              element: <AIGenerationScreen />,
-            },
-            {
-              path: "flashcards",
-              element: <FlashcardsLayout />,
-            },
-            {
-              path: "mindmap",
-              element: <MindMapView />,
-            },
+            { index: true, element: <AIGenerationScreen /> },
+            { path: "flashcards", element: <FlashcardsLayout /> },
+            { path: "mindmap", element: <MindMapView /> },
           ],
         },
-        {
-          path: "/rooms",
-          element: (
-            <div
-              style={{
-                padding: "2em",
-                fontSize: "1.5em",
-                color: "var(--color-text-light)",
-              }}
-            >
-              Rooms Coming Soon!
-            </div>
-          ),
-        },
-        {
-          path: "/resource-hub",
-          element: (
-            <div
-              style={{
-                padding: "2em",
-                fontSize: "1.5em",
-                color: "var(--color-text-light)",
-              }}
-            >
-              Resource Hub Coming Soon!
-            </div>
-          ),
-        },
+
+        // Rooms
+        { path: "rooms", element: <RoomList /> },
+        { path: "room/:roomId", element: <VideoCall /> },
+
+        // Resource Hub
+        { path: "resources", element: <AllResources /> },
+        { path: "resources/pdf/:id", element: <PdfReader /> },
+        { path: "upload", element: <UploadPage /> },
+        { path: "my-library", element: <MyLibrary /> },
+        { path: "groups", element: <GroupResources /> },
+        { path: "trending", element: <TrendingPage /> },
       ],
     },
   ]);
